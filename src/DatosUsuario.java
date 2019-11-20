8cfbaf7d266ef9bf69a91aa983b421b574a7dacf
pitah <pitah@LAPTOP-891IKFBD>
2019-11-20 20:16:34 +0100

    Ultima actualizacion del trabajo

---
+++ src/DatosUsuario.java
@@ -0,0 +1,17 @@
+
+public class DatosUsuario {
+
+	// Clase para manejar datos de el Login, por ahora es un basico ejemplo 
+	
+	public int  Existe(String nombre, String Contraseña) {
+		
+		if(nombre.equals("jesus") || nombre.equals("pedro")) {
+			if (Contraseña.equals("1234a")) {
+				return 1;
+			}else 
+				return 0;
+		}else return 0 ;
+	
+	}
+
+}
--- src/VentanaLogin.java
+++ src/VentanaLogin.java
@@ -1,10 +1,14 @@
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VentanaLogin {
 
-	private JFrame frame;
+	public  JFrame Login;
+	private JTextField user;
+	private JTextField pass;
 
 	/**
 	 * Launch the application.
@@ -14,7 +18,7 @@ public class VentanaLogin {
 			public void run() {
 				try {
 					VentanaLogin window = new VentanaLogin();
-					window.frame.setVisible(true);
+					window.Login.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -33,9 +37,55 @@ public class VentanaLogin {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Login = new JFrame();
+		Login.setBounds(100, 100, 1012, 590);
+		Login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Login.getContentPane().setLayout(null);
+		
+		user = new JTextField();
+		user.setBounds(335, 177, 282, 30);
+		Login.getContentPane().add(user);
+		user.setColumns(10);
+		
+		pass = new JTextField();
+		pass.setBounds(335, 248, 282, 26);
+		Login.getContentPane().add(pass);
+		pass.setColumns(10);
+		
+		JLabel lblUsuario = new JLabel("USUARIO ");
+		lblUsuario.setBounds(149, 177, 174, 16);
+		Login.getContentPane().add(lblUsuario);
+		
+		JLabel lblContrasea = new JLabel("Contraseña ");
+		lblContrasea.setBounds(149, 248, 174, 16);
+		Login.getContentPane().add(lblContrasea);
+		
+		JButton btnNewButton = new JButton("Entrtar ");
+		btnNewButton.addActionListener(new BtnNewButtonActionListener());
+	
+	
+		btnNewButton.setBounds(409, 330, 126, 49);
+		Login.getContentPane().add(btnNewButton);
 	}
-
+	
+		
+	
+	
+	private class BtnNewButtonActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			
+		// Codigo correspondiente al de entrar mas o menos Usuario jesus o pedro y contraseña 1234a para los dos 
+			// Codigo sucio hay que mejorarlo 
+			DatosUsuario d =new DatosUsuario();
+		if (d.Existe(user.getText(), pass.getText())==1) {
+			VentanaPagina Vp= new VentanaPagina();
+			Vp.frame.setVisible(true);
+			
+			Login.dispose();//
+		}else {
+			JOptionPane.showMessageDialog(null, "DATOS INCORRECTOS");
+		}
+			
+		}
+	}
 }
