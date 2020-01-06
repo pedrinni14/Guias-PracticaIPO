a76abc6ff8ad816d824d020e039cfcf446a01e81
jesusgamtello <56297001+jesusgamtello@users.noreply.github.com>
2020-01-06 23:17:11 +0100

    cambios en ofertas again

--- src/presentacion/PanelOfertas.java
+++ src/presentacion/PanelOfertas.java
@@ -16,6 +16,8 @@ import javax.swing.ImageIcon;
 import java.awt.TextField;
 import java.awt.CardLayout;
 import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PanelOfertas extends JPanel {
 	private JPanel pnlOfertas;
@@ -25,11 +27,7 @@ public class PanelOfertas extends JPanel {
 	private JList list;
 	private JScrollPane scrollPane_1;
 	private JList list_1;
-	private JButton btnAadir_1;
-	private JButton btnModificar_1;
 	private JButton btnBorrar_1;
-	private JButton btnAadir;
-	private JButton btnModificar;
 	private JButton btnBorrar;
 	private JTextArea textArea;
 	private JTextField textField;
@@ -47,7 +45,7 @@ public class PanelOfertas extends JPanel {
 		pnlOfertas = new JPanel();
 		add(pnlOfertas);
 		GridBagLayout gbl_pnlOfertas = new GridBagLayout();
-		gbl_pnlOfertas.columnWidths = new int[]{0, 70, 69, 49, 0, 0};
+		gbl_pnlOfertas.columnWidths = new int[]{0, 79, 79, 79, 0, 0};
 		gbl_pnlOfertas.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
 		gbl_pnlOfertas.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		gbl_pnlOfertas.rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
@@ -66,34 +64,18 @@ public class PanelOfertas extends JPanel {
 		list.setBorder(new TitledBorder(null, "Ofertas", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		scrollPane.setViewportView(list);
 		
-		btnAadir = new JButton("Añadir");
-		GridBagConstraints gbc_btnAadir = new GridBagConstraints();
-		gbc_btnAadir.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnAadir.insets = new Insets(0, 0, 5, 5);
-		gbc_btnAadir.gridx = 1;
-		gbc_btnAadir.gridy = 2;
-		pnlOfertas.add(btnAadir, gbc_btnAadir);
-		
-		btnModificar = new JButton("Modificar");
-		GridBagConstraints gbc_btnModificar = new GridBagConstraints();
-		gbc_btnModificar.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnModificar.insets = new Insets(0, 0, 5, 5);
-		gbc_btnModificar.gridx = 2;
-		gbc_btnModificar.gridy = 2;
-		pnlOfertas.add(btnModificar, gbc_btnModificar);
-		
 		btnBorrar = new JButton("Borrar");
 		GridBagConstraints gbc_btnBorrar = new GridBagConstraints();
 		gbc_btnBorrar.fill = GridBagConstraints.HORIZONTAL;
 		gbc_btnBorrar.insets = new Insets(0, 0, 5, 5);
-		gbc_btnBorrar.gridx = 3;
+		gbc_btnBorrar.gridx = 2;
 		gbc_btnBorrar.gridy = 2;
 		pnlOfertas.add(btnBorrar, gbc_btnBorrar);
 		
 		pnlPromociones = new JPanel();
 		add(pnlPromociones);
 		GridBagLayout gbl_pnlPromociones = new GridBagLayout();
-		gbl_pnlPromociones.columnWidths = new int[]{0, 0, 88, 0, 0, 0};
+		gbl_pnlPromociones.columnWidths = new int[]{0, 79, 79, 79, 53, 0};
 		gbl_pnlPromociones.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
 		gbl_pnlPromociones.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		gbl_pnlPromociones.rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
@@ -112,38 +94,28 @@ public class PanelOfertas extends JPanel {
 		list_1.setBorder(new TitledBorder(null, "Promociones", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		scrollPane_1.setViewportView(list_1);
 		
-		btnAadir_1 = new JButton("Añadir");
-		GridBagConstraints gbc_btnAadir_1 = new GridBagConstraints();
-		gbc_btnAadir_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnAadir_1.insets = new Insets(0, 0, 5, 5);
-		gbc_btnAadir_1.gridx = 1;
-		gbc_btnAadir_1.gridy = 2;
-		pnlPromociones.add(btnAadir_1, gbc_btnAadir_1);
-		
-		btnModificar_1 = new JButton("Modificar");
-		GridBagConstraints gbc_btnModificar_1 = new GridBagConstraints();
-		gbc_btnModificar_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnModificar_1.insets = new Insets(0, 0, 5, 5);
-		gbc_btnModificar_1.gridx = 2;
-		gbc_btnModificar_1.gridy = 2;
-		pnlPromociones.add(btnModificar_1, gbc_btnModificar_1);
-		
 		btnBorrar_1 = new JButton("Borrar");
 		GridBagConstraints gbc_btnBorrar_1 = new GridBagConstraints();
 		gbc_btnBorrar_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_btnBorrar_1.insets = new Insets(0, 0, 5, 5);
-		gbc_btnBorrar_1.gridx = 3;
+		gbc_btnBorrar_1.gridx = 2;
 		gbc_btnBorrar_1.gridy = 2;
 		pnlPromociones.add(btnBorrar_1, gbc_btnBorrar_1);
 		
 		panel_2 = new JPanel();
 		add(panel_2);
 		GridBagLayout gbl_panel_2 = new GridBagLayout();
-		gbl_panel_2.columnWidths = new int[]{68, 81, 85, 82, 0, 0};
+		gbl_panel_2.columnWidths = new int[]{68, 96, 90, 82, 0};
 		gbl_panel_2.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
-		gbl_panel_2.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_2.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		gbl_panel_2.rowWeights = new double[]{0.0, 0.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel_2.setLayout(gbl_panel_2);
+		
+		btnGuardar = new JButton("Modificar");
+		btnGuardar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		
 		textField = new JTextField();
 		textField.setText("");
@@ -151,6 +123,7 @@ public class PanelOfertas extends JPanel {
 		textField.setEditable(false);
 		textField.setColumns(10);
 		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.gridwidth = 2;
 		gbc_textField.insets = new Insets(0, 0, 5, 5);
 		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField.gridx = 1;
@@ -160,21 +133,19 @@ public class PanelOfertas extends JPanel {
 		button = new JButton("");
 		button.setIcon(new ImageIcon(PanelOfertas.class.getResource("/presentacion/user-2.png")));
 		GridBagConstraints gbc_button = new GridBagConstraints();
-		gbc_button.insets = new Insets(0, 0, 5, 5);
-		gbc_button.gridx = 2;
+		gbc_button.insets = new Insets(0, 0, 5, 0);
+		gbc_button.gridx = 3;
 		gbc_button.gridy = 1;
 		panel_2.add(button, gbc_button);
 		
 		textArea = new JTextArea();
 		GridBagConstraints gbc_textArea = new GridBagConstraints();
 		gbc_textArea.gridwidth = 3;
-		gbc_textArea.insets = new Insets(0, 0, 5, 5);
+		gbc_textArea.insets = new Insets(0, 0, 5, 0);
 		gbc_textArea.fill = GridBagConstraints.BOTH;
 		gbc_textArea.gridx = 1;
 		gbc_textArea.gridy = 2;
 		panel_2.add(textArea, gbc_textArea);
-		
-		btnGuardar = new JButton("Guardar");
 		GridBagConstraints gbc_btnGuardar = new GridBagConstraints();
 		gbc_btnGuardar.fill = GridBagConstraints.HORIZONTAL;
 		gbc_btnGuardar.insets = new Insets(0, 0, 5, 5);
@@ -182,7 +153,11 @@ public class PanelOfertas extends JPanel {
 		gbc_btnGuardar.gridy = 3;
 		panel_2.add(btnGuardar, gbc_btnGuardar);
 		
-		btnEnviar = new JButton("Enviar");
+		btnEnviar = new JButton("Añadir");
+		btnEnviar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		GridBagConstraints gbc_btnEnviar = new GridBagConstraints();
 		gbc_btnEnviar.fill = GridBagConstraints.HORIZONTAL;
 		gbc_btnEnviar.insets = new Insets(0, 0, 5, 5);
@@ -193,7 +168,7 @@ public class PanelOfertas extends JPanel {
 		btnCancelar = new JButton("Cancelar");
 		GridBagConstraints gbc_btnCancelar = new GridBagConstraints();
 		gbc_btnCancelar.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnCancelar.insets = new Insets(0, 0, 5, 5);
+		gbc_btnCancelar.insets = new Insets(0, 0, 5, 0);
 		gbc_btnCancelar.gridx = 3;
 		gbc_btnCancelar.gridy = 3;
 		panel_2.add(btnCancelar, gbc_btnCancelar);
